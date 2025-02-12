b827c645388f127d295e262879dcdfddd2d64a23
Arina <aarinayamatina2008@gmail.com>
2025-02-12 11:11:57 +0300

    commit

---
+++ task1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int weight;
+    printf("Введите вашу массу тела\n");
+    scanf("%d", &weight);
+    
+    if (weight < 60 | weight > 90) 
+        printf("Вы не можете пройти отбор");
+    else 
+        printf("Вы успешно прошли отбор");
+
+}
